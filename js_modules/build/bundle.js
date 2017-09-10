2fbd19797f06e898a896efbecd6058b37452eb6c
Jayce77 <xjaycex@hotmail.com>
2017-09-09 20:36:38 -0400

    Added image_viewer.js

--- js_modules/build/bundle.js
+++ js_modules/build/bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -73,11 +73,10 @@
 "use strict";
 
 
-var sum = function sum(a, b) {
-  return a + b;
-};
+var image = document.createElement('img');
+image.src = 'http://lorempixel.com/400/400';
 
-module.exports = sum;
+document.body.appendChild(image);
 
 /***/ },
 /* 1 */
@@ -86,9 +85,31 @@ module.exports = sum;
 "use strict";
 
 
-var sum = __webpack_require__(0);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var sum = function sum(a, b) {
+  return a + b;
+};
 
-var total = sum(10, 5);
+exports.default = sum;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _sum = __webpack_require__(1);
+
+var _sum2 = _interopRequireDefault(_sum);
+
+__webpack_require__(0);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var total = (0, _sum2.default)(10, 5);
 console.log(total);
 
 /***/ }
